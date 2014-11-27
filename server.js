fb70f7641c77039e2d024b33e7e3a56e491b0068
Drew Blaisdell <drew.blaisdell@gmail.com>
2014-11-26 21:55:06 -0800

    Removed files/dependencies from the old game.

--- server.js
+++ server.js
@@ -1,19 +1,14 @@
 var express = require('express');
-var bodyParser = require('body-parser');
 var app = express();
 var http = require('http').Server(app);
 var path = require('path');
-var mongoose = require('mongoose');
 var morgan = require('morgan');
-var io = require('socket.io')(http);
-var analyticsServer = require('./app/analyticsServer')(io);
-
-mongoose.connect('mongodb://localhost/test');
-
-analyticsServer.init();
 
 app.use(morgan('dev'));
-app.use(bodyParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-http.listen(3000);
+app.set('port', 3000);
+
+http.listen(app.get('port'), function() {
+  console.log('drewblaisdell.com fileserver started on port '+ app.get('port'));
+});

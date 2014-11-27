c961c59377db162fb60b3d16479f85df7c119ff8
Drew Blaisdell <drew.blaisdell@gmail.com>
2014-11-27 01:34:26 -0800

    Now using an environment port.

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var morgan = require('morgan');
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.set('port', 3000);
+app.set('port', process.env.PORT || 3000);
 
 http.listen(app.get('port'), function() {
   console.log('drewblaisdell.com fileserver started on port '+ app.get('port'));

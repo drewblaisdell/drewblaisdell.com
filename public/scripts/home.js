31cbafe08f7b7c9d24b7a686574be88ab42c3f56
Drew Blaisdell <drew.blaisdell@gmail.com>
2014-11-27 00:40:39 -0800

    Added email/phone number.

---
+++ public/scripts/home.js
@@ -0,0 +1,41 @@
+window.onload = function() {
+  var title = document.getElementById('title'),
+    emailAddress = document.getElementById('email-address'),
+    emailIcon = document.getElementById('email-icon'),
+    phoneIcon = document.getElementById('phone-icon'),
+    phoneNumber = document.getElementById('phone-number'),
+    iconEvents = {
+      showEmail: function(event) {
+        title.className = 'activated hidden';
+        emailAddress.className = 'visible';
+        phoneNumber.className = '';
+        event.preventDefault();
+      },
+
+      hideEmail: function(event) {
+        title.className = 'activated';
+        emailAddress.className = '';
+        event.preventDefault();
+      },
+
+      showNumber: function(event) {
+        title.className = 'activated hidden';
+        phoneNumber.className = 'visible';
+        emailAddress.className = '';
+        event.preventDefault();
+      },
+
+      hideNumber: function(event) {
+        title.className = 'activated';
+        phoneNumber.className = '';
+        event.preventDefault();
+      }
+    };
+
+  emailIcon.addEventListener('mouseenter', iconEvents.showEmail);
+  emailIcon.addEventListener('mouseleave', iconEvents.hideEmail);
+
+  phoneIcon.addEventListener('mouseenter', iconEvents.showNumber);
+  phoneIcon.addEventListener('mouseleave', iconEvents.hideNumber);
+  phoneIcon.addEventListener('click', iconEvents.showNumber);
+};
